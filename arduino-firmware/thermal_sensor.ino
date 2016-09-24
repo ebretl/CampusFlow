6dd259496abd527959e2c08f493b96efa9733f0b
Evan Bretl <evanbretl4@gmail.com>
2016-09-24 11:13:40 -0400

    implemented average tracking in thermal_sensor

--- arduino-firmware/thermal_sensor.ino
+++ arduino-firmware/thermal_sensor.ino
@@ -1,6 +1,18 @@
 
-float temperature(int pinTempSensor) {
-  int B=4275;
+float temperature() {
+  return averageTemperature;
+}
+
+void updateTemperature () {
+  int newVal = getRawTemperature();
+  updateAverage(newVal);
+}
+
+
+
+int B=4275;
+
+float getRawTemperature(int pinTempSensor) {
   float a = analogRead(pinTempSensor ) * 3.3 / 5;
   
   //find resistance
@@ -10,5 +22,19 @@ float temperature(int pinTempSensor) {
   //convert to temperature via datasheet
   float temperature=1.0/(log(R/100000.0)/B+1/298.15)-273.15;
   return temperature;
+}
+
+int averageTemperature;
+unsigned long thermTotal;
+int thermIndex;
+int [64] thermArray;
+
+void updateAverage(int newVal) {
+  thermTotal += newVal;
+  thermTotal -= thermArray[thermIndex];
+  thermArray[thermIndex] = newVal;
+  
+  thermIndex = (thermIndex + 1) & 63;
+  averageTemperature = thermTotal >> 6;
 }
 
