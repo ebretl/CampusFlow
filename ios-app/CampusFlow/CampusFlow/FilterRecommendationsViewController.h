438972b759bdd81a154d793572525c2592d7aea5
Ellie <ellie@lawn-128-61-24-39.lawn.gatech.edu>
2016-09-25 08:59:54 -0400

    Final iOS updates

---
+++ ios-app/CampusFlow/CampusFlow/FilterRecommendationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilterRecommendationsViewController.h
+//  CampusFlow
+//
+//  Created by Ellie on 9/25/16.
+//  Copyright © 2016 HackGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterRecommendationsViewController : UIViewController
+
+@end
---
+++ ios-app/CampusFlow/CampusFlow/RecResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecResultsViewController.h
+//  CampusFlow
+//
+//  Created by Ellie on 9/25/16.
+//  Copyright © 2016 HackGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecResultsViewController : UITableViewController
+
+@end
